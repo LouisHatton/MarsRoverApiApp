b2ce02f55e11270d79f5f1a144d1530621893d91
Louis Haton <71732103+LouisHatton@users.noreply.github.com>
2021-06-03 16:26:11 +0100

    Organising images into 4 columns
    Also updated hard-coded API request to sol 2: less images need to load when testing.

--- src/main/java/com/application/marsapi/service/MarsRoverApiService.java
+++ src/main/java/com/application/marsapi/service/MarsRoverApiService.java
@@ -12,7 +12,7 @@ public class MarsRoverApiService {
 	public MarsRoverApiResponse getRoverData () {
 		RestTemplate rt = new RestTemplate();
 		
-		ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DEMO_KEY", MarsRoverApiResponse.class);
+		ResponseEntity<MarsRoverApiResponse> response = rt.getForEntity("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=2&api_key=DEMO_KEY", MarsRoverApiResponse.class);
 		return response.getBody();
 	}
 	
